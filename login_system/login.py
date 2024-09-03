57fd7d925d3b38bb910f82da33bf20518c386d27
Hamzah-Amir <hamzaamir7651@gmail.com>
2024-09-03 15:04:58 +0500

    Delete login_system directory

--- login_system/login.py
+++
@@ -1,8 +0,0 @@
-data = {input("Enter your username: "):input("Enter your password"),input("Enter Your username"):input("Enter your password")}
-
-if (data != "HI"):
-
-    print("Account is created")
-else:
-    print("Try again")
-    
